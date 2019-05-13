38fda41ac6f1f06eebd3fa63a922044b8769503c
Tony Boyles <Anthony@Boyles.cc>
2019-05-13 12:21:07 -0400

    Provided /demo a substantial Face lift Now uses Material Design for Boostrap to get that crispy Android-app feel. Also did a bunch of PWA stuff so you can install it like an app

---
+++ demo/sw.js
@@ -0,0 +1,24 @@
+var CACHE = 'TidyTree';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll([
+        './',
+        'index.html',
+        'life.nwk',
+        'saveSvgAsPng.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(evt){
+  evt.respondWith(fetch(evt.request).catch(function(){
+    return caches.open(CACHE).then(function(cache){
+      return cache.match(evt.request).then(function(matching){
+        return matching || Promise.reject('no-match');
+      });
+    });
+  }));
+});
