fa0ece21a860ac1a630be25bf2a4ee35bf166ae2
Danielle Callan <dcallan@upenn.edu>
2023-12-06 13:08:39 -0500

    Revert "switch to using ids rather than guids to color by"
    This reverts commit 206a7621aca41cd355cbdcc97074cadcd56f4c2f.

--- src/main.js
+++ src/main.js
@@ -386,8 +386,8 @@ function findNodeColor(node, colorOptions) {
   }
  
   let nodeList = colorOptions.nodeList;
-  
-  if (nodeList && nodeList.includes(node.data.id)) {
+
+  if (nodeList && nodeList.includes(node.data._guid)) {
     // yellowish
     return colorOptions.highlightColor ?? "#feb640";
   } else {
@@ -413,7 +413,7 @@ function findBranchColor(link, colorOptions) {
   let childLeafNodes = getAllLeaves(source);
   
   let allChildLeafNodesInNodeList = childLeafNodes.every(child =>
-    colorOptions.nodeList?.includes(child.data.id)
+    colorOptions.nodeList?.includes(child.data._guid)
   );
  
   if (allChildLeafNodesInNodeList) {
@@ -574,7 +574,7 @@ TidyTree.prototype.redraw = function () {
   let links = g
     .select("g.tidytree-links")
     .selectAll("g.tidytree-link")
-    .data(source(this.hierarchy).links(), l => l.source.data.id + ':' + l.target.data.id);
+    .data(source(this.hierarchy).links(), l => l.source.data._guid + ':' + l.target.data._guid);
 
   links.join(
     enter => {
@@ -658,7 +658,7 @@ TidyTree.prototype.redraw = function () {
   let nodes = g
     .select("g.tidytree-nodes")
     .selectAll("g.tidytree-node")
-    .data(this.hierarchy.descendants(), d => d.data.id);
+    .data(this.hierarchy.descendants(), d => d.data._guid);
   nodes.join(
     enter => {
       let nt = nodeTransformers[this.type][this.layout];
@@ -1222,13 +1222,13 @@ TidyTree.prototype.setRuler = function (show) {
 };
 
 /**
- * Retrieves the IDs of the nodes in the TidyTree.
+ * Retrieves the GUIDs of the nodes in the TidyTree instance.
  *
- * @param {boolean} leavesOnly - Indicates whether to retrieve only the IDs of leaf nodes.
- * @param {function} predicate - An optional predicate function to filter the nodes.
- * @return {Array} An array containing the IDs of the nodes that meet the specified conditions.
+ * @param {boolean} leavesOnly - Whether to retrieve GUIDs only for leaf nodes.
+ * @param {function} predicate - A function that returns true if the node should be included
+ * @return {Array} An array of GUIDs of the nodes.
  */
-TidyTree.prototype.getNodeIDs = function (leavesOnly, predicate) {
+TidyTree.prototype.getNodeGUIDs = function (leavesOnly, predicate) {
   let nodeList = this.parent
     .select("svg")
     .selectAll("g.tidytree-node-leaf circle")
@@ -1241,14 +1241,14 @@ TidyTree.prototype.getNodeIDs = function (leavesOnly, predicate) {
       ._groups[0];
   }
 
-  let nodeIDs = [];
+  let nodeGUIDs = [];
   for (const node of nodeList.values()) {
     if (!predicate || predicate(node)) {
-      nodeIDs.push(node.__data__.data.id);
+      nodeGUIDs.push(node.__data__.data._guid);
     }
   }
- 
-  return nodeIDs;
+
+  return nodeGUIDs;
 }
 
 /**
